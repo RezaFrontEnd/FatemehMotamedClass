8b2c3fa6436371bd8dbf3647b2107dcbf68994d0
RezaWarrior <reza.ahmadi350@gmail.com>
2026-02-08 19:52:42 +0330

    Delete 17.js

--- 17.js
+++
@@ -1,9 +0,0 @@
-let i=1;
-const result = setInterval(function()
-{
-    console.log("message loged each 1 second " + i);
-    i = i+1; 
-    setTimeout
-    (function(){clearInterval(result);},10000)
-},2000);
-
