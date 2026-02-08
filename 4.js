3af97940d3db64b02101be6efcf14c3128dc2ed7
RezaWarrior <reza.ahmadi350@gmail.com>
2026-02-08 19:53:11 +0330

    Delete 4.js

--- 4.js
+++
@@ -1,6 +0,0 @@
-let objectWithNestedObject = {};
-objectWithNestedObject.nestedObject = {};
-objectWithNestedObject.nestedObject.a = 10;
-const objectB = "b";
-objectWithNestedObject.nestedObject[objectB] = true;
-console.log(objectWithNestedObject);
