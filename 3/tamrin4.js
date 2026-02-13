efc19a47ce93071d12fd88669b6a2f09ae742518
Reza-ahmadi-FrontEnd <44983522+RezaFrontEnd@users.noreply.github.com>
2026-02-14 00:27:16 +0330

    create_tamrin_for

---
+++ 3/tamrin4.js
@@ -0,0 +1,10 @@
+function NameChapTedad(message,count)
+{
+   let result = message.repeat(count)
+   if(message)
+        {
+            console.log(result)
+            
+        }
+}
+NameChapTedad("hello\n",5)
---
+++ 3/tamrin5.js
@@ -0,0 +1,9 @@
+function listUsers(a=[])
+{
+    for(let x=0;x<a.length;x++)
+        {
+           
+            console.log(`${x}.${a[x]}`)
+        }    
+}
+listUsers(["Reza","ali","mohammad"])
